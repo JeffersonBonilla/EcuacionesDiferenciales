9d05365bdfee880223e962f48b0fa7e7994ebe81
JeffersonBonilla <bm0749042022@unab.edu.sv>
2025-10-21 11:46:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -179,3 +179,6 @@ def solve(req: SolveRequest):
         steps=steps,
         raw_sympy=sol_str
     )
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
